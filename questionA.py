dfc36464270878bdd95d50aaa5a5eda5ccc29a0c
Antoine Girard Dubreuil <antoine.dubreuil91@gmail.com>
2019-06-06 19:09:35 -0400

    questionA done

---
+++ questionA.py
@@ -0,0 +1,44 @@
+#############################
+#		Question A			#
+#############################
+
+class LineOverlap():
+	def __init__(self):
+		self.x1=0
+		self.x2=0
+		self.x3=0
+		self.x4=0
+
+	def main(self):
+		self.inputName()
+
+		if(self.line_verification() == True):
+			self.find_overlap()
+
+	def inputName(self):
+		self.x1 = input("\nx1:")
+		self.x2 = input("x2:")
+		self.x3 = input("x3:")
+		self.x4 = input("x4:")
+
+	def find_overlap(self):
+		entete='\nL1('+self.x1+','+self.x2+')'+ 'and '+ 'L2('+self.x3+','+self.x4+') '
+
+		if (self.x2 >= self.x3 or self.x4 <= self.x1):
+			print(entete + 'overlap')
+		else:
+			print(entete + 'do not overlap')
+
+	def line_verification(self):
+		if (self.x1 >= self.x2):
+			print("\nL1 is not valid")
+			return False
+		elif(self.x3 >= self.x4):
+			print("\nL2 is not valid")
+			return False
+		else:
+			return True
+
+if __name__ == '__main__':
+	lineoverlap=LineOverlap()
+	lineoverlap.main()
